7a51fc4b43177e270ac5c4e124dde6c49fa09581
Kazumasa-Kan <terrier.clan.d@gmail.com>
2025-08-08 03:42:53 +0900

    add 415-c wip

---
+++ 415/c/ans.cpp
@@ -0,0 +1,19 @@
+# include <bits/stdc++.h>
+# include <string>
+# include <vector>
+using namespace std;
+
+int main(){
+    int N, T;
+    string s;
+    cin >> N;
+
+    for (int i = 0 ; i < N ; i++){
+        cin >> T;
+        cin >> s;
+        
+
+    }
+
+    return 0;
+}
