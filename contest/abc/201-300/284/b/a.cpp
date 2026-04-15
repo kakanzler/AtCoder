143b602415227a9e36f00dc64c34a55fa4064294
Kazumasa-Kan <terrier.clan.d@gmail.com>
2026-04-15 13:15:11 +0900

    solved : abc-284-b

--- contest/abc/201-300/284/b/a.cpp
+++ contest/abc/201-300/284/b/a.cpp
@@ -54,112 +54,18 @@ using ar2 = array<ll, 2>;
 
 //--------3.コード短縮化とか---------
 #define rep(i,n) for(ll i = 0; i < (n); ++i)
-#define rrep(i,n) for(ll i = 1; i <= (n); ++i)
-#define drep(i,n) for(ll i = (n)-1; i >= 0; --i)
-#define nfor(i,s,n) for(ll i=s;i<n;i++)//i=s,s+1...n-1 ノーマルfor
-#define dfor(i,s,n) for(ll i = (s)-1; i>=n;i--)//s-1スタートでnまで落ちる
-
-#define nall(a) a.begin(),a.end()
-#define rall(a) a.rbegin(),a.rend()
-
-#define chmax(x,y) x = max(x,y)
-#define chmin(x,y) x = min(x,y)
-
-#define pb push_back
-#define eb emplace_back
-#define em emplace
-#define pob pop_back
-
-
-#define YES cout<<"Yes"<<endl
-#define NO cout<<"No"<<endl
-#define YN {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
-#define dame cout<<-1<<endl
-
-
-#define vc_unique(v) v.erase( unique(v.begin(), v.end()), v.end() );
-#define vc_rotate(v) rotate(v.begin(),v.begin()+1,v.end());
-
-#define pop_cnt(s) ll(popcount(uint64_t(s)))
-
-#define next_p(v) next_permutation(v.begin(),v.end())
-
-//if (regex_match(s, regex("")))YN;//文字列sの判定を行う。コメントアウトを外して「""」の中に判定する内容を入れる
-
-//-------------------------------
-
-
-
-
-//---------4.グリッド系----------
-vl dx = { 1,0,-1,0 };//vl dx={1,1,0,-1,-1,-1,0,1};
-vl dy = { 0,1,0,-1 };//vl dy={0,1,1,1,0,-1,-1,-1};
-
-bool out_grid(ll i, ll j, ll h, ll w) {//trueならcontinue
-    return (!(0 <= i && i < h && 0 <= j && j < w));
-}
-
-#define vvl_kaiten(v) {ll n = size(v);vvl nx(n,vl(n));rep(i,n)rep(j,n)nx[j][n-i-1]=v[i][j];swap(nx,v);}//時計回りに90°回転
-//#define vvl_kaiten(v) {ll n = size(v);vvl nx(n,vl(n));rep(i,n)rep(j,n)nx[n-j-1][i]=v[i][j];swap(nx,v);}//反時計周りに90°回転
-
-#define vs_kaiten(v) {ll n = size(v);vs nx(n,string(n,'.'));rep(i,n)rep(j,n)nx[j][n-i-1]=v[i][j];swap(nx,v);}//文字列版 時計回りに90°回転
-//#define vs_kaiten(v) {ll n = size(v);vs nx(n,string(n,'.'));rep(i,n)rep(j,n)nx[n-j-1][i]=v[i][j];swap(nx,v);}//文字列版　反時計周りに90°回転
-
-
-#define vvl_tenti(v) {ll n = size(v);vvl nx(n,vl(n));rep(i,n)rep(j,n)nx[j][i]=v[i][j];swap(nx,v);}
-#define vs_tenti(v) {ll n = size(v); vs nx(n, string(n,'.')); rep(i, n)rep(j, n)nx[j][i] = v[i][j]; swap(nx, v);}
-
-//--------------------------------
-
-
-
-
-//-----------5.数学系--------------
-#define yu_qurid(x,y) ((x)*(x)+(y)*(y))//ユークリッド距離 sqrtはしてないなので注意
-#define mannhattan(x1,x2,y1,y2) (abs(x1-x2)+abs(y1-y2)) // マンハッタン距離 = |x1-x2|+|y1-y2|
-
-template<class T>T tousa_sum1(T l, T d, T r) {//初項,公差,末項 で総和を求める
-    T wide = (r - l) / d + 1;
-    return (l + r) * wide / 2;
-}
-template<class T>T tousa_sum2(T a, T d, T n) {//初項,交差,項数 で総和を求める
-    return (a * 2 + d * (n - 1)) * n / 2;
-}
-ll kousa_kousuu(ll l, ll r, ll d) {//初項,末項,交差 で等差数列の項数を求める
-    return (r - l) / d + 1;
-}
-mint touhi_sum(mint a, mint r, ll n) {//初項,公比,項数で等比数列の総和を求める
-    if (r == 1) {
-        return a * n;
-    }
-    mint bunsi = a * (r.pow(n) - mint(1));
-    mint bunbo = r - 1;
-    return bunsi / bunbo;
-}
-
-ll nc2(ll x) { return x * (x - 1) / 2; }
-ll nc3(ll x) { return x * (x - 1) * (x - 2) / 6; }
-
-//----------------------------------------------
-
-
-
-
-//-----------6.デバックや出力系------------------
-void print(ld x) { printf("%.20Lf\n", x); }
-
-void mukou_debug(vvl to, bool yukou) {//GRAPH × GRAPH用の無向グラフを出力する
-    ll n = size(to); ll cnt = 0;//辺の本数
-    vc<pair<ll, ll>>v; rep(i, n)for (ll t : to[i])  if (i < t || yukou)cnt++, v.eb(i + 1, t + 1);//有向グラフなら全部OK、違うなら無向なのでf<tのみ見る、using Pのやつを別のにしたいときのためにPを使わずにpair<ll,ll>にしてる
-    cout << n << ' ' << cnt << endl; for (auto [f, t] : v)cout << f << ' ' << t << endl;
-}
-
-#define vc_cout(v){ll n = size(v);rep(i,n)cout<<v[i]<<endl;}//一次元配列を出力する
-#define vv_cout(v){ll n = size(v);rep(i,n){rep(j,size(v[i])){cout<<v[i][j]<<' ';}cout<<endl;}}//二次元配列を出力する
-
-//----------------------------------------------
 
 void solve() {
+    ll t; cin >> t;
+    rep(i, t){
+        ll n; cin >> n;
+        ll ans=0, a=0;
+        rep(j, n) {
+            cin >> a;
+            if (a % 2) ans++;
+        }
+        cout << ans << endl;
+    }
     return;
 }
 
