a6082dbcc66dede239c36bf88501068410e58601
Kazumasa-Kan <terrier.clan.d@gmail.com>
2026-05-14 05:29:41 +0900

    init : EDPC-G

---
+++ contest/EducationDPContest/G-LongestPath/a.cpp
@@ -0,0 +1,28 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG//[]で配列外参照をするとエラーにしてくれる。上下のやつがないとTLEになるので注意 ABC311Eのサンプル4みたいなデバック中のTLEは防げないので注意
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+
+template<typename T> using vc = vector<T>;//prioriy_queueに必要なのでここにこれ書いてます
+
+//-------------1.型系---------------
+using ll = long long;
+
+//--------3.コード短縮化とか---------
+#define rep(i,n) for(ll i = 0; i < (n); ++i)
+
+void solve() {
+
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
