ffe739d25722e9448b340bfe4eef0f5bf4607495
Kazumasa-Kan <terrier.clan.d@gmail.com>
2026-07-05 00:39:34 +0900

    solved : abc-465-a/b/c/d

---
+++ contest/abc/401-500/465/b/a.cpp
@@ -0,0 +1,58 @@
+#ifndef ONLINE_JUDGE
+#define _GLIBCXX_DEBUG//[]で配列外参照をするとエラーにしてくれる。上下のやつがないとTLEになるので注意 ABC311Eのサンプル4みたいなデバック中のTLEは防げないので注意
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+
+
+template<typename T> using vc = vector<T>;//prioriy_queueに必要なのでここにこれ書いてます
+template<typename T> using vv = vc<vc<T>>;
+
+//-------------1.型系---------------
+using ll = long long;
+using ull = unsigned long long;
+ll INF = 2e18;
+
+using mint = modint998244353;
+//using mint = modint1000000007;
+//using mint = modint;//使うときはコメントアウトを外す
+//mint::set_mod(m);//使うときはコメントアウトを外す
+
+//-------------2.配列系--------------
+using vl = vc<ll>; using vvl = vv<ll>; using vvvl = vv<vl>; using vvvvl = vv<vvl>;
+using vs = vc<string>; using vvs = vv<string>;
+
+
+//--------3.コード短縮化とか---------
+#define rep(i,n) for(ll i = 0; i < (n); ++i)
+
+#define YES cout<<"Yes"<<endl
+#define NO cout<<"No"<<endl
+#define YN {cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}// if(a==b)YN;
+#define dame cout<<-1<<endl
+//----------------------------------------------
+
+void solve() {
+    ll x, y, l, r, a, b; cin >> x >> y >> l >> r >> a >> b;
+    bool ain = false, bin = false, f = false;
+    if (l <= a && a < r) ain = true;
+    if (l < b && b <= r) bin = true;
+    if (a < l && r < b) f = true;
+
+    if      ( ain &&  bin)   cout << (b - a) * x << endl;
+    else if (!ain && !bin && !f)   cout << (b - a) * y << endl;
+    else if (!ain && !bin && f)    cout << (l - a) * y + (r - l) * x  + (b - r) * y << endl;
+    else if (!ain &&  bin)   cout << (l - a) * y + (b-l) * x << endl;
+    else if ( ain && !bin)   cout << (r - a) * x + (b-r) * y << endl;
+
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
